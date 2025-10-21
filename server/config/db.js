23da2ef76934a0cebec00f865ba480fdb55f6be1
Chefkwami <sitsofefeyi28@gmail.com>
2025-10-21 19:59:45 +0100

    bug fix in deployment

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,14 @@ import mongoose from "mongoose";
 const connectToDB = async() => {
 
     try {
+        if (!process.env.MONGO_URI) {
+            throw new Error("Mongodb is not defined in environment variables")
+        }
 
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log("MongoDB connected Successfully")
 
     } catch (error) {
